aced00d1f06918fa413f4108afec1a3315c722a8
Alfred Nico <alfrfednciotsu@gmail.com>
2024-04-14 19:55:28 +0300

    install module calandar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'planning';
+
+
+  /* Add Event Form */
+  addEventForm = this.fb.group({
+    title: ['', [Validators.required]]
+  });
+  submitted = false;
+  //Add user form actions
+
+  get f() { return this.addEventForm.controls; }
+
+ onSubmit() {
+
+  this.submitted = true;
+  // stop here if form is invalid and reset the validations
+  this.addEventForm.controls['title'].setValidators([Validators.required]);
+  this.addEventForm.controls['title'].updateValueAndValidity();
+  if (this.addEventForm.invalid) {
+      return;
+  }
+}
+constructor(private fb: FormBuilder){}
+  title = 'angularadmintemplates';
+  calendarOptions!: CalendarOptions;
+
+  ngOnInit() {
+    this.calendarOptions = {
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick.bind(this),
+      events: [
+        { title: 'event 1', date: '2020-11-05' },
+        { title: 'event 2', date: '2020-06-30' }
+      ]
+    };
+    //Add User form validations
+  }
+//Show Modal with Forn on dayClick Event
+  handleDateClick(arg: any) {
+    // $("#myModal").modal("show");
+    // $(".modal-title, .eventstarttitle").text("");
+    // $(".modal-title").text("Add Event at : "+arg.dateStr);
+    // $(".eventstarttitle").text(arg.dateStr);
+    console.log('args = ', arg);
+
+
+  }
+  //Hide Modal PopUp and clear the form validations
+  hideForm(){
+    this.addEventForm.patchValue({ title : ""});
+    this.addEventForm.controls['title'].clearValidators();
+    this.addEventForm.controls['title'].updateValueAndValidity();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { ReactiveFormsModule } from '@angular/forms';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin
+]);
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
